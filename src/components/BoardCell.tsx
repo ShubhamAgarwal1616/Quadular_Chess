b43cb68167bd96cd7d43f038fc169e1e04b2b462
shubham agarwal <shubham.agarwal@johnlewis.co.uk>
2023-03-05 10:35:28 +0530

    create a separate component for chess piece

--- src/components/BoardCell.tsx
+++ src/components/BoardCell.tsx
@@ -2,35 +2,14 @@ import {Cell} from "../classes/Cell";
 import {FC} from "react";
 import styles from "./BoardCell.module.scss";
 import cx from "classnames";
-import {CellColor, GradientColor, PieceColor, PieceType, ThroneSide} from "../classes/constants";
+import {CellColor, GradientColor, ThroneSide} from "../classes/constants";
 import {GradientBackground} from "./GradientBackground";
+import {ChessPiece} from "./ChessPiece";
 
 interface CellProps {
     cell: Cell;
 }
 export const BoardCell: FC<CellProps> = ({cell}) => {
-    const getPiece = (cell: Cell) => {
-        switch (cell.piece?.type) {
-            case PieceType.Pawn: return <>&#9823;</>
-            case PieceType.Queen: return <>&#9819;</>
-            case PieceType.Rook: return <>&#9820;</>
-            case PieceType.Knight: return <>&#9822;</>
-            case PieceType.King: return <>&#9818;</>
-            case PieceType.Bishop: return <span className={styles.fasIcon}>&#xf43a;</span>
-            case PieceType.Prince: return <span className={styles.fasIcon}>&#xf445;</span>
-            // case PieceType.YoungKing: return <>&#xf43f;</>
-        }
-    }
-
-    const getPieceColorClass = (cell: Cell) => {
-        switch (cell.piece?.color) {
-            case PieceColor.ORANGE: return styles.orangePiece
-            case PieceColor.YELLOW: return styles.yellowPiece
-            case PieceColor.BLACK: return styles.blackPiece
-            case PieceColor.WHITE: return styles.whitePiece
-        }
-    }
-
     const getThroneSidesClass = (cell: Cell): string => {
         if(cell.gradientColor === GradientColor.GRADIENT_YELLOW) {
             return cell.throneSides === ThroneSide.Left ? styles.leftBottomThrone : styles.rightBottomThrone;
@@ -57,9 +36,7 @@ export const BoardCell: FC<CellProps> = ({cell}) => {
                 <GradientBackground cell={cell} />
             )}
             {cell.piece && (
-                <div className={cx('fas', styles.pieceIcon, getPieceColorClass(cell))}>
-                    {getPiece(cell)}
-                </div>
+                <ChessPiece cell={cell} />
             )}
         </button>
     )
---
+++ src/components/ChessPiece.tsx
@@ -0,0 +1,39 @@
+import cx from "classnames";
+import styles from "./ChessPiece.module.scss";
+import {Cell} from "../classes/Cell";
+import {PieceColor, PieceType} from "../classes/constants";
+import {FC} from "react";
+
+interface ChessPieceProps {
+    cell: Cell;
+}
+
+export const ChessPiece: FC<ChessPieceProps> = ({cell}) => {
+    const getPiece = (cell: Cell) => {
+        switch (cell.piece?.type) {
+            case PieceType.Pawn: return <>&#9823;</>
+            case PieceType.Queen: return <>&#9819;</>
+            case PieceType.Rook: return <>&#9820;</>
+            case PieceType.Knight: return <>&#9822;</>
+            case PieceType.King: return <>&#9818;</>
+            case PieceType.Bishop: return <span className={styles.fasIcon}>&#xf43a;</span>
+            case PieceType.Prince: return <span className={styles.fasIcon}>&#xf445;</span>
+            // case PieceType.YoungKing: return <>&#xf43f;</>
+        }
+    }
+
+    const getPieceColorClass = (cell: Cell) => {
+        switch (cell.piece?.color) {
+            case PieceColor.ORANGE: return styles.orangePiece
+            case PieceColor.YELLOW: return styles.yellowPiece
+            case PieceColor.BLACK: return styles.blackPiece
+            case PieceColor.WHITE: return styles.whitePiece
+        }
+    }
+
+    return (
+        <div className={cx('fas', styles.pieceIcon, getPieceColorClass(cell))}>
+            {getPiece(cell)}
+        </div>
+    )
+}
